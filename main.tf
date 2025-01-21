2d87395b68d79cd97964031f140058dfc0ec95ab
sagarhdange03 <sagarhdange03@gmail.com>
2025-01-21 16:51:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.83.0"
     }
+   
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
