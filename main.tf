fd7cf2be85668c71888a97c4da1537503d57300a
sagarhdange03 <sagarhdange03@gmail.com>
2025-01-21 17:04:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 # main.tf
+provider "aws" {
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+}
+
 terraform {
   required_providers {
     aws = {
