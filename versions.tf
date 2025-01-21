0eb1ff3cd69af97ad4b249dfaeceb2b63d489854
sagarhdange03 <sagarhdange03@gmail.com>
2025-01-21 10:20:23 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,10 +2,7 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "= 5.84"
-    }
+   
     external = {
       source  = "hashicorp/external"
       version = ">= 1.0"
