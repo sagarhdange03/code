d487fcb032a013d09f89e5c123226956d7aac927
sagarhdange03 <sagarhdange03@gmail.com>
2025-01-21 17:11:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,14 +16,7 @@ terraform {
       version = "5.83.0"
     }
    
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
+   
   }
 }
 
